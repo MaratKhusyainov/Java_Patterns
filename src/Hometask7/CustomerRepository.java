34b6665789b1d63c6bb5b6606bcaa0e42ae4ed26
MaratKhusyainov <khusyainov.mar@yandex.ru>
2021-05-07 18:06:15 +0300

    Выполнено домашнее задание № 7

---
+++ src/Hometask7/CustomerRepository.java
@@ -0,0 +1,10 @@
+package Hometask7;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+}
